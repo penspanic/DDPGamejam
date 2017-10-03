93c3011cbbc62ee57f1538c318e99befdc778ef9
Youngchangoon <youngchangoon521@gmail.com>
2017-10-03 17:14:18 +0900

    [김영찬] 메인씬 이펙트 추가.

---
+++ Assets/Scripts/Main_Scene/AnimationObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DDP.Main_Scene
+{
+	public class AnimationObject : MonoBehaviour
+	{
+		public virtual void InitBeforeAni()
+		{
+			
+		}
+
+		public virtual IEnumerator PlayShowAnimation()
+		{
+			yield return null;
+		}
+	}
+}
