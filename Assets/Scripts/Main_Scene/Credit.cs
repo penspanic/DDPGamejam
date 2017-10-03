ba1ba276f958b56c31bdd7dda224ac2ae1bc3147
penspanic <penspanic@gmail.com>
2017-10-04 00:23:41 +0900

    [박근희] 크레딧 UI 완료

---
+++ Assets/Scripts/Main_Scene/Credit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DDP.Main_Scene
+{
+    public class Credit : Singleton<Credit>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        public void Show()
+        {
+
+        }
+
+        public void Hide()
+        {
+
+        }
+    }
+}
